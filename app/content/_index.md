1ed29c3065d09f478cb22973da392f36fd6f4b37
Pete Coles <pete@fffunction.co>
2017-11-20 23:59:59 +0000

    First commit

---
+++ app/content/_index.md
@@ -0,0 +1,14 @@
+---
+title: "These bearded mermen have made a calendar for mental health"
+sub: "We’ve come a long way since the original calendar girls"
+draft: false
+---
+
+That’s right; witness the glory that is the 2018 MerB’ys Calendar, brought to you by Canada’s Newfoundland and Labrador (NL) Beard and Moustache Club.
+
+What happens at a beard and moustache club? Well, the first rule seems to be ‘bring your merman tail’.
+
+![Alt text](http://www.placecage.com/g/600/450 "Optional title")
+_Caption as emphasis_
+
+Because between the handlebar waxing and goatee combing, these burly men are stripping down and flaunting their fins in a novel take on the classic charity calendar shoot. We know how important regular and up-to-date reports on road, rail and other modes of transport incidents are for the UK audience. We have therefore been working on incorporating this information within the Local News offering online. You will be able to find all the latest severe travel incidents on our local live services and on news stories which feature travel disruption. We also hope to feed travel updates into the personalised news services we are developing.
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp         = require("gulp"),
+    sass         = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer")
+    sassGlob     = require('gulp-sass-glob');
+    hash         = require("gulp-hash"),
+    del          = require("del")
+
+
+
+    gulp.task("scss", function () {
+        del(["app/static/css/**/*"])
+        gulp.src("app/themes/judy/static/css/**/*.scss")
+          .pipe(sassGlob())
+          .pipe(sass({outputStyle : "expanded"}))
+          .pipe(autoprefixer({browsers : ["last 20 versions"]}))
+          .pipe(gulp.dest("app/static/css"))
+          
+          .pipe(sass({outputStyle : "compressed"}))
+          .pipe(hash())
+          .pipe(gulp.dest("app/static/css"))
+          //Create a hash map
+          .pipe(hash.manifest("hash.json"))
+          //Put the map in the data directory
+          .pipe(gulp.dest("app/data/css"))
+    })
+
+    // Hash javascript
+    gulp.task("js", function () {
+        del(["app/static/js/**/*"])
+        gulp.src("app/themes/judy/static/js/**/*")
+            .pipe(hash())
+            .pipe(gulp.dest("static/js"))
+            .pipe(hash.manifest("hash.json"))
+            .pipe(gulp.dest("app/data/js"))
+    })
+
+    // Watch asset folder for changes
+    gulp.task("watch", ["scss", "js"], function () {
+        gulp.watch("app/themes/judy/static/css/**/*", ["scss"])
+        gulp.watch("app/themes/judy/static/js/**/*", ["js"])
+    })
+
+    // Copy stuff for patterns
+    gulp.task("copy", function() {
+      del(["patterns/elements/**/*"])
+      del(["patterns/theme/css/**/*"])
+      gulp.src("app/themes/judy/layouts/partials/components/**/*")
+        .pipe(gulp.dest("patterns/components"))
+      gulp.src("app/static/css/**/*")
+          .pipe(gulp.dest("patterns/theme/css"))
+    })
+
+    // --------------------------
+    // Task runners
+    // --------------------------
+
+    // Default (watch)
+    gulp.task("default", ["watch"])
+
+    // Copy stuff to patterns
+    gulp.task("patterns", ["copy"])
